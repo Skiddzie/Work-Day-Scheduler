165ddcb45fba6a9c3c141a08ce6f14c3760bf590
Skiddzie <aidanthe1@gmail.com>
2022-04-09 13:17:27 -0400

    trimmed down the code and added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 var currentHour = moment().hour();
 var timeSlots = $(".hour-content");
+//creates an empty array if the object tasks has not been created yet
 var allText = JSON.parse(localStorage.getItem("tasks")) || [];
 
+//when called an object will be commited to local storage using the user inputted text
 var saveContent = function(text, time) {
     var task = {
         text,
@@ -11,13 +13,17 @@ var saveContent = function(text, time) {
 
     localStorage.setItem("tasks", JSON.stringify(allText));
   };
-var loadContent = function(){
-    for (var i = 0; i < allText.length; i++) {
-        var textVal = $("#"+allText[i].time).val(allText[i].text);
-        console.log(textVal);
-    }
-}
-loadContent();
+
+
+// var loadContent = function(){
+//     for (var i = 0; i < allText.length; i++) {
+//         var textVal = $("#"+allText[i].time).val(allText[i].text);
+//         console.log(textVal);
+//     }
+// }
+
+
+//loops through every timeslot to check if it's before, during, or after the current time
 var checkTime = function() {
     for (var i = 0; i < timeSlots.length; i++) {
         var slot = timeSlots[i];
@@ -34,51 +40,19 @@ var checkTime = function() {
        }
 };
 
-
-checkTime();
+//when the save button is clicked, it's respective sibling element selected and then called for with saveContent()
 $(".save-btn").on("click", function() {
     // console.log(this);
     var content = $(this).siblings('.hour-content');
 
     var userInput = content.val();
     var sectionId = content.attr("id");
-    // console.log(sectionId);
-    // console.log(userInput);
+
     saveContent(userInput, sectionId);
 });
-// $(".hour-content").on("click", function() {
-//     console.log(this);
-//     var text = $(this)
-//       .text()
-//       .trim();
-//     var textInput = $("<textarea>")
-//       .val(text);
-//     $(this).replaceWith(textInput);
-//     // textInput.trigger("focus");
-//   });
-  
-//   $(".hour-content").on("blur", function() {
-//     var text = $(this)
-//       .val()
-//       .trim();
-  
-//     var status = $(this)
-//       .closest(".list-group")
-//       .attr("id")
-//       .replace("list-","");
-  
-//     var index = $(this)
-//       .closest(".list-group-item")
-//       .index();
-  
-//     tasks[status][index].text = text;
-//     saveTasks();
-  
-//     var taskP = $("<p>")
-//       .addClass("m-1")
-//       .text(text);
-  
-//     $(this).replaceWith(taskP);
-//   });
-// checkTime();
-// setInterval(checkTime, 3600000);
+
+// loadContent();
+//checks the time when the page is loaded
+checkTime();
+//checks the time every hour
+setInterval(checkTime, 3600000)
